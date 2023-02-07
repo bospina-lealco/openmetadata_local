cdefb741100089ec10a4f6a2331676f01dee4a1f
bospina-lealco <bospina@leal.co>
2023-02-06 19:00:45 -0500

    changes

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
-kubectl create namespaces opendata
-kubectl apply -f opendata-storage.yaml -n opendata
+#kind create cluster --name airflow-cluster --config kind-cluster.yaml 
+#kubectl create namespace opendata
+#kubectl apply -f opendata-storage.yaml -n opendata
 kubectl create secret generic mysql-secrets --from-literal=openmetadata-mysql-password=openmetadata_password -n opendata
 kubectl create secret generic airflow-secrets --from-literal=openmetadata-airflow-password=admin -n opendata
 kubectl create secret generic airflow-mysql-secrets --from-literal=airflow-mysql-password=airflow_pass -n opendata
-helm install openmetadata open-metadata/openmetadata --values values.yaml -n opendata
-helm install openmetadata-dependencies open-metadata/openmetadata-dependencies --values values-dependencies.yaml -n opendata
+#helm upgrade -i openmetadata open-metadata/openmetadata --values values.yaml -n opendata
+#helm upgrade -i openmetadata-dependencies open-metadata/openmetadata-dependencies --values values-dependencies.yaml -n opendata
